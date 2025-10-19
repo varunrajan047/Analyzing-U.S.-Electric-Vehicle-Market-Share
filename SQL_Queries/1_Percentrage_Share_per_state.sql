be1e0e8da5b89c8d7ae1334907d0029f4ee77acf
varunrajan047 <varunrajan0494@gmail.com>
2025-10-18 22:33:35 -0700

    percentage calc error corrected

--- SQL_Queries/1_Percentrage_Share_per_state.sql
+++ SQL_Queries/1_Percentrage_Share_per_state.sql
@@ -15,10 +15,10 @@ SELECT
     electric,
     ROUND((electric*1.0/total_vehicles)*100,2) AS ev_percentage,
     phev,
-    ROUND((phev*1.0/total_vehicles),2) AS phev_percentage,
+    ROUND((phev*1.0/total_vehicles)*100,2) AS phev_percentage,
     hev,
-    ROUND((hev*1.0/total_vehicles),2) AS hev_percentage,
+    ROUND((hev*1.0/total_vehicles)*100,2) AS hev_percentage,
     gasoline,
-    ROUND((gasoline*1.0/total_vehicles),2) AS gas_percentage
+    ROUND((gasoline*1.0/total_vehicles)*100,2) AS gas_percentage
 FROM sum_calc;
 
