26b98261dc8c1b3a5449fdf9ae9d24ef3070e073
varunrajan047 <varunrajan0494@gmail.com>
2025-10-18 22:29:46 -0700

    first commit

---
+++ SQL_Queries/1_Percentrage_Share_per_state.sql
@@ -0,0 +1,24 @@
+-- Calculate the percentage of EVs, PHEVs, HEVs, and Gasoline vehicles for each state.
+
+WITH sum_calc AS 
+(SELECT
+    state,
+    electric,
+    phev,
+    hev,
+    gasoline,
+    (electric + phev + hev + biodiesel + e85 + cng + propane + hydrogen + methanol + gasoline + diesel + unknown_fuel) AS total_vehicles
+FROM  ev_regdata
+)
+SELECT
+    state,
+    electric,
+    ROUND((electric*1.0/total_vehicles)*100,2) AS ev_percentage,
+    phev,
+    ROUND((phev*1.0/total_vehicles),2) AS phev_percentage,
+    hev,
+    ROUND((hev*1.0/total_vehicles),2) AS hev_percentage,
+    gasoline,
+    ROUND((gasoline*1.0/total_vehicles),2) AS gas_percentage
+FROM sum_calc;
+
---
+++ SQL_Queries/2_top_5_ev_states.sql
@@ -0,0 +1,16 @@
+-- Identify the top 5 states with the highest EV adoption rate (EVs as a % of all registered vehicles).
+
+WITH total_ev_reg_calc AS 
+(
+SELECT
+    state,
+    electric,
+    (electric + phev + hev + biodiesel + e85 + cng + propane + hydrogen + methanol + gasoline + diesel + unknown_fuel) AS total_reg
+FROM ev_regdata
+)
+SELECT 
+    state,
+    electric,
+    ROUND((electric*1.0/total_reg),2) AS percentage_share
+FROM total_ev_reg_calc
+ORDER BY percentage_share DESC;
